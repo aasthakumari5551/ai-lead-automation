408a6fb66dee149e0a783e597a61deda02cc8357
aasthakumari5551 <aasthakumari551@gmail.com>
2026-05-17 23:00:39 +0530

    Initial project setup with React frontend and Express backend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import LeadForm from "./components/LeadForm";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+      <LeadForm />
+    </div>
+  );
+}
+
+export default App;
